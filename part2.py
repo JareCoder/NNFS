9f26d6e059978abd133a5edc06aac1ec7f534ad9
Jesse Lappalainen <jesse.m.lappalainen@gmail.com>
2025-07-10 01:30:31 +0300

    Add new input

--- part2.py
+++ part2.py
@@ -1,6 +1,7 @@
-inputs = [1, 2, 3] # Madeup values from the book
-weights = [0.2, 0.8, -0.5] # Madeup values from the book
-bias = 2 # Madeup value from the book
+# Madeup values from the book
+inputs = [1, 2, 3, 2.5]
+weights = [0.2, 0.8, -0.5, 1.0]
+bias = 2
 
 output = 0
 for i in range(len(inputs)):
