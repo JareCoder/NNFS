80cc46718d4f4004b9ebcadb576899693e3b2ab5
Jesse Lappalainen <jesse.m.lappalainen@gmail.com>
2025-07-10 02:23:44 +0300

    Throw in numpy and do fun vector math

--- part3/part3.py
+++ part3/part3.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 # Madeup values from the book
 inputs = [1, 2, 3, 2.5]
 weights = [[0.2, 0.8, -0.5, 1.0],
@@ -5,6 +7,12 @@ weights = [[0.2, 0.8, -0.5, 1.0],
            [-0.26, -0.27, 0.17, 0.87]]
 biases = [2, 3, 0.5]
 
+# Weights first to index by the weight sets. If you flip the order, you'll get shape mismatch and errors.
+# Visualisation: https://youtu.be/tMrbN67U9d4?t=1112
+output = np.dot(weights, inputs) + biases
+print(output)  # Should print [4.8, 1.21, 2.385]
+
+# Manual way
 layer_outputs = []
 for neuron_weights, neuron_bias in zip(weights, biases):
     neuron_output = 0
